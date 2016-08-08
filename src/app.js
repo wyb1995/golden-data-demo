9c40da273cacead4a797c53e4654c66c071e6092
wyb1995 <wy_b1995@163.com>
2016-08-08 16:57:16 +0800

    golden-data-demo-second

--- src/app.js
+++ src/app.js
@@ -1,12 +1,34 @@
 const App = React.createClass({
     render: function () {
-        return <div>App</div>
+        return <div>
+            {this.props.children}
+        </div>
     }
 });
+
+const Editor = React.createClass({
+    render: function () {
+        return <div>
+            <ReactRouter.Link to="/previewer">previewer</ReactRouter.Link>
+            Editor
+        </div>
+    }
+});
+
+const previewer = React.createClass({
+    render: function () {
+        return <div>
+            <ReactRouter.Link to="/">Editor</ReactRouter.Link>
+            previewer
+        </div>
+    }
+})
 
 ReactDOM.render(
     <ReactRouter.Router>
         <ReactRouter.Route path="/" component={App}>
+            <ReactRouter.IndexRoute component={Editor} />
+            <ReactRouter.Route path="previewer" component={previewer} />
         </ReactRouter.Route>
     </ReactRouter.Router>
 ,document.getElementById('content'));
