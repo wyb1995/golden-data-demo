97b03b4c6c569b9a9121c68e01031ba2f45a0ed3
wyb1995 <wy_b1995@163.com>
2016-08-08 18:01:19 +0800

    golden-data-demo-third

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 const App = React.createClass({
+    getInitialState: function () {
+        return {
+            elements: ['date']
+        }
+    },
+    removeElement: function (index) {
+        const elements = this.state.elements;
+        elements.splice(index,1);
+        this.setState({elements});
+    },
     render: function () {
         return <div>
-            {this.props.children}
+            {this.props.children && React.cloneElement(this.props.children, {
+                elements: this.state.elements,
+                onRemove: this.removeElement
+            })}
         </div>
     }
 });
@@ -10,8 +23,30 @@ const Editor = React.createClass({
     render: function () {
         return <div>
             <ReactRouter.Link to="/previewer">previewer</ReactRouter.Link>
-            Editor
+            <LeftPanel elements={this.props.elements} onRemove={this.props.onRemove}/>
+            <RightPanel />
         </div>
+    }
+});
+
+const LeftPanel = React.createClass({
+    remove: function (index) {
+        this.props.onRemove(index);
+    },
+    render: function () {
+        const elements = this.props.elements.map((ele, index) => {
+            return <div key={index}>
+                <input type={ele}/>
+                <button onClick={this.remove.bind(this, index)}>X</button>
+            </div>
+        });
+        return <div>{elements}</div>
+    }
+});
+
+const RightPanel = React.createClass({
+    render: function () {
+        return <div>rightPanel</div>
     }
 });
 
@@ -27,8 +62,8 @@ const previewer = React.createClass({
 ReactDOM.render(
     <ReactRouter.Router>
         <ReactRouter.Route path="/" component={App}>
-            <ReactRouter.IndexRoute component={Editor} />
-            <ReactRouter.Route path="previewer" component={previewer} />
+            <ReactRouter.IndexRoute component={Editor}/>
+            <ReactRouter.Route path="previewer" component={previewer}/>
         </ReactRouter.Route>
     </ReactRouter.Router>
-,document.getElementById('content'));
+    , document.getElementById('content'));
